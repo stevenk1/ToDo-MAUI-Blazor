97927591ae0e14172a8a9904f806fd356f207f5d
Steven Kennis <steven.kennis@cegeka.com>
2022-07-07 21:44:00 +0200

    init

---
+++ ToDo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ ToDo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ToDo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
